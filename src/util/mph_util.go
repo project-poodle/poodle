4c1edb933d775cc045194b689b87e8390aa287d3
Tahitian Shell <gsk390@gmail.com>
2020-05-21 15:20:57 -0700

    Minimum Perfect Hash

--- src/util/mph_util.go
+++ src/util/mph_util.go
@@ -36,70 +36,6 @@ import (
 	"unsafe"
 )
 
-
-// This file contains an optimized murmur3 32-bit implementation tailored for
-// our specific use case. See https://en.wikipedia.org/wiki/MurmurHash.
-
-// A murmurSeed is the initial state of a Murmur3 hash.
-type murmurSeed uint32
-
-const (
-	c1      = 0xcc9e2d51
-	c2      = 0x1b873593
-	r1Left  = 15
-	r1Right = 32 - r1Left
-	r2Left  = 13
-	r2Right = 32 - r2Left
-	m       = 5
-	n       = 0xe6546b64
-)
-
-// hash computes the 32-bit Murmur3 hash of s using ms as the seed.
-func (ms murmurSeed) hash(b []byte) uint32 {
-	h := uint32(ms)
-	l := len(b)
-	numBlocks := l / 4
-	var blocks []uint32
-	header := (*reflect.SliceHeader)(unsafe.Pointer(&blocks))
-	header.Data = (*reflect.SliceHeader)(unsafe.Pointer(&b)).Data
-	header.Len = numBlocks
-	header.Cap = numBlocks
-	for _, k := range blocks {
-		k *= c1
-		k = (k << r1Left) | (k >> r1Right)
-		k *= c2
-		h ^= k
-		h = (h << r2Left) | (h >> r2Right)
-		h = h*m + n
-	}
-
-	var k uint32
-	ntail := l & 3
-	itail := l - ntail
-	switch ntail {
-	case 3:
-		k ^= uint32(b[itail+2]) << 16
-		fallthrough
-	case 2:
-		k ^= uint32(b[itail+1]) << 8
-		fallthrough
-	case 1:
-		k ^= uint32(b[itail])
-		k *= c1
-		k = (k << r1Left) | (k >> r1Right)
-		k *= c2
-		h ^= k
-	}
-
-	h ^= uint32(l)
-	h ^= h >> 16
-	h *= 0x85ebca6b
-	h ^= h >> 13
-	h *= 0xc2b2ae35
-	h ^= h >> 16
-	return h
-}
-
 // A Table is an immutable hash table that provides constant-time lookups of key
 // indices using a minimal perfect hash.
 type MPHTable struct {
@@ -191,3 +127,68 @@ type bySize []indexBucket
 func (s bySize) Len() int           { return len(s) }
 func (s bySize) Less(i, j int) bool { return len(s[i].vals) > len(s[j].vals) }
 func (s bySize) Swap(i, j int)      { s[i], s[j] = s[j], s[i] }
+
+
+// Below code contains an optimized murmur3 32-bit implementation tailored for
+// our specific use case. See https://en.wikipedia.org/wiki/MurmurHash.
+
+// A murmurSeed is the initial state of a Murmur3 hash.
+type murmurSeed uint32
+
+const (
+	c1      = 0xcc9e2d51
+	c2      = 0x1b873593
+	r1Left  = 15
+	r1Right = 32 - r1Left
+	r2Left  = 13
+	r2Right = 32 - r2Left
+	m       = 5
+	n       = 0xe6546b64
+)
+
+// hash computes the 32-bit Murmur3 hash of s using ms as the seed.
+func (ms murmurSeed) hash(b []byte) uint32 {
+	h := uint32(ms)
+	l := len(b)
+	numBlocks := l / 4
+	var blocks []uint32
+	header := (*reflect.SliceHeader)(unsafe.Pointer(&blocks))
+	header.Data = (*reflect.SliceHeader)(unsafe.Pointer(&b)).Data
+	header.Len = numBlocks
+	header.Cap = numBlocks
+	for _, k := range blocks {
+		k *= c1
+		k = (k << r1Left) | (k >> r1Right)
+		k *= c2
+		h ^= k
+		h = (h << r2Left) | (h >> r2Right)
+		h = h*m + n
+	}
+
+	var k uint32
+	ntail := l & 3
+	itail := l - ntail
+	switch ntail {
+	case 3:
+		k ^= uint32(b[itail+2]) << 16
+		fallthrough
+	case 2:
+		k ^= uint32(b[itail+1]) << 8
+		fallthrough
+	case 1:
+		k ^= uint32(b[itail])
+		k *= c1
+		k = (k << r1Left) | (k >> r1Right)
+		k *= c2
+		h ^= k
+	}
+
+	h ^= uint32(l)
+	h ^= h >> 16
+	h *= 0x85ebca6b
+	h ^= h >> 13
+	h *= 0xc2b2ae35
+	h ^= h >> 16
+	return h
+}
+
