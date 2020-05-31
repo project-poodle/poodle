c44034a10a0b23fbb02d11a7e49ba5a8bc7ae09e
Tahitian Shell <gsk390@gmail.com>
2020-05-31 01:14:52 -0700

    update

--- src/collection/object.go
+++ src/collection/object.go
@@ -11,14 +11,18 @@ import (
 // Interfaces
 ////////////////////////////////////////////////////////////////////////////////
 
+// Base interface for all objects
 type IObject interface {
 }
 
+// interface for printable
 type IPrintable interface {
+
 	// Print writes string representation of the object to Writer,
 	//    with specified indent, child objects if any,
 	//    separated by return characters
 	Print(w io.Writer, indent int)
+
 	// ToString returns string representation of the object, without indent, or return character
 	ToString() string
 }
